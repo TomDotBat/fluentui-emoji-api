b9a5661a20256eeed7e7c66f23c9a4eed71cc2eb
Tom O'Sullivan <tom@tomdotbat.dev>
2022-08-12 15:36:39 +0100

    refactor: move the updater logic into a class

--- src/index.js
+++ src/index.js
@@ -28,82 +28,33 @@
 */
 
 import config from "./config.js";
-import Git from "./Git.js";
+import Updater from "./Updater.js";
 
-import fs from "fs/promises";
-import {promisify} from "util";
+const autoUpdate = async () => {
+	const updater = await Updater.initialize();
 
-const timeout = promisify(setTimeout);
-
-const git = await Git.initialize();
-
-if (git.isInstalled) {
-	console.log(`Detected git version ${git.version}, proceeding.`)
-}
-else {
-	console.error("git installation was not detected, please install git and try again.")
-	process.exit(1);
-}
-
-try {
-	await fs.access(config.get("CLONE_LOCATION"));
-	console.log("FluentUI emoji repository found, checking for updates...");
-
-	git.workingDirectory = config.get("CLONE_LOCATION");
-
-	await git.fetch();
-
-	const localCommit = await git.parseRevision("HEAD");
-	const remoteCommit = await git.parseRevision("origin/HEAD");
-
-	if (localCommit === remoteCommit) {
-		console.log(`The local repository is up-to-date with the remote (${localCommit}).`)
+	if (await updater.isRepositoryCloned()) {
+		if (config.get("AUTO_UPDATE")) {
+			console.log("FluentUI emoji repository found, checking for updates...");
+		
+			if (await updater.isRepositoryUpToDate()) {
+				console.log(`The local repository is up-to-date with the remote.`);
+			}
+			else {
+				console.log("The local repository is not up-to-date with the remote:");
+				await updater.pull();
+				console.log("The local repository is now up-to-date.");
+			}
+		}
+		else {
+			console.log("FluentUI emoji repository found.");
+		}
 	}
 	else {
-		console.log("The local repository is not up-to-date with the remote:");
-		console.log("Local Revision: " + localCommit);
-		console.log("Remote Revision: " + remoteCommit);
-
-		let isPulling = true;
-	
-		git.pull()
-			.then(() => isPulling = false)
-			.catch((err) => {
-				console.error("\nUpdate failed.", err);
-				process.exit(1);
-			});
-		
-		process.stdout.write("Pulling latest changes from the remote.");
-		
-		while (isPulling) {
-			process.stdout.write(".");
-			await timeout(1000);
-		}
-
-		console.log("The local repository is now up-to-date.")
+		console.log("FluentUI emoji repository not found.");
+		await updater.clone();
+		console.log("Repository clone complete.");
 	}
-}
-catch (err) {
-	if (err.code !== "ENOENT") {
-		console.error("The FluentUI emoji repository is inaccessible.", err)
-		process.exit(1);
-	}
+};
 
-	let isCloning = true;
-	
-	git.clone(config.get("REPOSITORY_URL"), config.get("CLONE_LOCATION"))
-		.then(() => isCloning = false)
-		.catch((err) => {
-			console.error("\nRepository clone failed.", err);
-			process.exit(1);
-		});
-	
-	process.stdout.write("FluentUI emoji repository not found, cloning from remote.");
-	
-	while (isCloning) {
-		process.stdout.write(".");
-		await timeout(1000);
-	}
-	
-	console.log("Repository clone complete.");
-}
+await autoUpdate();
