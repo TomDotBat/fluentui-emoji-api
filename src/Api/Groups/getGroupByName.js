18e02360ebb0ff610e583a128cfddf80790c4026
Tom O'Sullivan <tom@tomdotbat.dev>
2022-08-14 15:50:04 +0100

    refactor: sort group routes into different files

---
+++ src/Api/Groups/getGroupByName.js
@@ -0,0 +1,43 @@
+/*
+	COPYRIGHT NOTICE:
+	© 2022 Thomas O'Sullivan - All rights reserved.
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+	FILE INFORMATION:
+	Name: getGroupByName.js
+	Project: FluentUI Emoji API
+	Author: Tom
+	Created: 14th August 2022
+*/
+
+export default function getGroupByName(req, res) {
+	const name = req.params.name;
+	const group = getEmojiGroupByName(name);
+
+	if (group) {
+		res.json(group);
+	}
+	else {
+		res.status(404).json({
+			status: 404,
+			message: `Group not found with name: ${name}`
+		});
+	}
+}
